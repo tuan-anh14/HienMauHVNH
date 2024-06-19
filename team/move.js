3008ed8f62d9aed4495012f56b09947c964a54c3
tuan-anh14 <thieutuananh14092k4@gmail.com>
2024-06-19 23:20:14 +0700

    fix font chữ, hình ảnh

--- team/move.js
+++ team/move.js
@@ -1,11 +1,11 @@
-var snippet = [].slice.call(document.querySelectorAll('.hover'));
+var snippet = [].slice.call(document.querySelectorAll(".hover"));
 if (snippet.length) {
   snippet.forEach(function (snippet) {
-    snippet.addEventListener('mouseout', function (event) {
-      if (event.target.parentNode.tagName === 'figure') {
-        event.target.parentNode.classList.remove('hover')
+    snippet.addEventListener("mouseout", function (event) {
+      if (event.target.parentNode.tagName === "figure") {
+        event.target.parentNode.classList.remove("hover");
       } else {
-        event.target.parentNode.classList.remove('hover')
+        event.target.parentNode.classList.remove("hover");
       }
     });
   });
